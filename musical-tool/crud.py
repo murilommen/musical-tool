dc2e9b26ff8dcd6a4d6b0406584b150382467774
murilommen <murilommen@gmail.com>
2021-08-08 19:19:18 -0300

    fixing imports

--- musical-tool/crud.py
+++ musical-tool/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-import models, schemas
+from . import models, schemas
 
 
 def get_user(db: Session, user_id: int):
--- musical-tool/get_files_list.py
+++ musical-tool/get_files_list.py
@@ -1,13 +1,12 @@
-page_token = None
-while True:
-    response = drive_service.files().list(q="mimeType='image/jpeg'",
-                                          spaces='drive',
-                                          fields='nextPageToken, files(id, name)',
-                                          pageToken=page_token).execute()
-    for file in response.get('files', []):
-        # Process change
-        print 'Found file: %s (%s)' % (file.get('name'), file.get('id'))
-    page_token = response.get('nextPageToken', None)
-    if page_token is None:
-        break
-
+# page_token = None
+# while True:
+#     response = drive_service.files().list(q="mimeType='image/jpeg'",
+#                                           spaces='drive',
+#                                           fields='nextPageToken, files(id, name)',
+#                                           pageToken=page_token).execute()
+#     for file in response.get('files', []):
+#         # Process change
+#         print 'Found file: %s (%s)' % (file.get('name'), file.get('id'))
+#     page_token = response.get('nextPageToken', None)
+#     if page_token is None:
+#         break

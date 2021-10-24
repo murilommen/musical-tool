9edb961423c2f116f5a7886c1ae5fe19979c0265
Murilo Menezes Mendonça <murilommen@gmail.com>
2021-10-24 16:46:06 -0300

    adding CORS middleware

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from musical_tool.routers import music
 
 
 app = FastAPI()
 app.include_router(music.router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
